e5b5113c4215e47a4e01332073bcd40618515fa1
pjgit2 <150532774+pjgit2@users.noreply.github.com>
2023-11-26 21:19:55 +0000

    Update hello.py
    Updated code

--- hello.py
+++ hello.py
@@ -1,1 +1,7 @@
-
+from flask import Flask
+app = Flask(__name__)
+@app.route("/")#URL leading to method
+def hello(): # Name of the method
+ return("Hello World!") #indent this line
+if __name__ == "__main__":
+ app.run(host='0.0.0.0', port='8080') # indent this line
