a78f7d982be332a4e4279ad9f3962c7f5a33fa41
pjgit2 <150532774+pjgit2@users.noreply.github.com>
2023-12-04 09:50:08 +0000

    Delete hello.py

--- hello.py
+++
@@ -1,7 +0,0 @@
-from flask import Flask
-app = Flask(__name__)
-@app.route("/")#URL leading to method
-def hello(): # Name of the method
- return("Hello World! You are here!") #indent this line
-if __name__ == "__main__":
- app.run(host='0.0.0.0', port='8080') # indent this line
