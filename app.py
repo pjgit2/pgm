9b72a4724b5adf0bf81b6b2c49b43a47edffedaa
SAGAR KAMBLE <73596435+Ksagar21@users.noreply.github.com>
2023-12-04 11:49:38 +0000

    ch 7

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import pyrebase
+from flask import Flask, render_template, request
 
 config = {
   "apiKey": "AIzaSyCv4mI1d_S63C00yn_OPQiX8qwPQHQl_oc",
@@ -17,8 +18,6 @@ db = firebase.database()
 
 #db.child("name").child("name").update({"name":"Paulu"})
 
-from flask import *
-from flask import Flask, render_template,request,redirect
 
 app = Flask(__name__)
 
@@ -26,15 +25,15 @@ app = Flask(__name__)
 def basic():
     if request.method == 'POST':
         if request.form['submit'] == 'add':
-            
             name =  request.form['name']
             db.child("todo").push(name)
             todo = db.child("todo").get()
-            to = todo.bal
+            to = todo.val()
             return render_template('index.html',t=to.values())
         elif request.form['submit'] == 'delete':
             db.child("todo").remove()
             return render_template('index.html')
+        return render.template('index.html')
 
 #if __name__ == '__main__':
   # app.run(debug=true)
@@ -45,6 +44,6 @@ if __name__ == '_main_':
 @app.route('/show')
 def show():
   
-  return render_template('show.html')
+  
 
 
