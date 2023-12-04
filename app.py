c15e247f904bb5ef55cbbcdb91ac8d8f71d592ff
SAGAR KAMBLE <73596435+Ksagar21@users.noreply.github.com>
2023-12-04 09:53:28 +0000

    change 1

--- app.py
+++ app.py
@@ -1,3 +1,20 @@
+import pyrebase
+
+config = {
+  "apiKey": "AIzaSyCv4mI1d_S63C00yn_OPQiX8qwPQHQl_oc",
+  "authDomain": "tesla-15857.firebaseapp.com",
+  "databaseURL": "https://tesla-15857-default-rtdb.firebaseio.com",
+  "projectId": "tesla-15857",
+  "storageBucket": "tesla-15857.appspot.com",
+  "messagingSenderId": "860727547248",
+  "appId": "1:860727547248:web:01f4b44b9039cc9895abb5",
+  "measurementId": "G-BPWDCB06B7"
+}
+
+firebase = pyrebase.initialize_app(config)
+
+
+
 from flask iport Flask
 
 app = Flask(__name__)
