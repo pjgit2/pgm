e337ab2126c749567af217da1753c72439cfce98
SAGAR KAMBLE <73596435+Ksagar21@users.noreply.github.com>
2023-12-04 09:28:27 +0000

    app.py

--- app.py
+++ app.py
@@ -4,3 +4,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
+  return "<h1> Welcome to the World of Flask Application Framework</h1>"
+
+@app.route('/about')
+def about():
+  return "<h1> All about the Page</h1> 
+          "<p> This Application was build under Flask Framework </p>"
+
+if __name__=="__main__";
+  app.run(debug=True)
