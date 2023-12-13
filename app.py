232839c0d33da5281f7c6127a8b25d127bb440e3
SAGAR KAMBLE <73596435+Ksagar21@users.noreply.github.com>
2023-12-13 19:17:24 +0000

    Port updated

--- app.py
+++ app.py
@@ -116,7 +116,7 @@ def edit(item_id):
         return redirect(url_for('admin'))
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=8000)
+    app.run(debug=True, host='0.0.0.0', port=8080)
 
 
 
